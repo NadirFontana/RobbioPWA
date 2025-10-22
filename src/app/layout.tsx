74498ec9d43d362a5b0673502bc7774fb00b167d
NadirFontana <fontananadir97@gmail.com>
2025-10-22 12:24:50 +0200

    PWA3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="it" suppressHydrationWarning>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#800000" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
