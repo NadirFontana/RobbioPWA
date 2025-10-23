a9137e3e6dbb24ad2258b9dbc39c103054a6f101
NadirFontana <fontananadir97@gmail.com>
2025-10-23 15:43:04 +0200

    TransparentFix2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 // Next.js 15: themeColor va in generateViewport
 export function generateViewport() {
   return {
-    themeColor: "#1d4ed8",
+    themeColor: "#0ea5e9",
   };
 }
 
@@ -28,40 +28,42 @@ export default function RootLayout({
       <head>
         {/* Meta per PWA su iOS/Android */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="mobile-web-app-capable" content="yes" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#1d4ed8" />
+        <meta name="theme-color" content="#0ea5e9" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
 
-        {/* Splash screen per iOS con icona trasparente */}
+        {/* Splash screen per iOS - immagine trasparente senza ombre */}
         <link
           rel="apple-touch-startup-image"
           href="/icon-192-transparent.png"
-          media="(device-width: 390px) and (device-height: 844px)
-                 and (-webkit-device-pixel-ratio: 3)
-                 and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/icon-192-transparent.png"
-          media="(device-width: 375px) and (device-height: 812px)
-                 and (-webkit-device-pixel-ratio: 3)
-                 and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/icon-192-transparent.png"
-          media="(device-width: 430px) and (device-height: 932px)
-                 and (-webkit-device-pixel-ratio: 3)
-                 and (orientation: portrait)"
+          media="(orientation: portrait)"
         />
       </head>
 
-      <body className="antialiased">{children}</body>
+      <body
+        className="antialiased"
+        style={{
+          backgroundColor: "#0ea5e9",
+          margin: 0,
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {/* Splash screen visiva mentre React carica */}
+        <div
+          id="splash"
+          style={{
+            background: "url('/icon-192-transparent.png') no-repeat center center / 192px 192px",
+            position: "absolute",
+            inset: 0,
+          }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
